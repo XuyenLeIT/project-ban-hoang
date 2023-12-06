4cc072673f16d1e6615b774192837ff5b311ba7e
XuyenLe <letuanxuyen92@gmail.com>
2023-12-06 15:43:39 +0700

    refactor css

--- resources/views/layouts/client_layout.blade.php
+++ resources/views/layouts/client_layout.blade.php
@@ -12,17 +12,17 @@
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row client__header">
+    <div class="container-fluid gx-0">
+        <div class="row client__header gx-0">
             <div class="client__header-contact">
                 <p>Hotline: 0948 629 345 - 0327 629 345 Facebook: facebook.com/tbcn.vn</p>
             </div>
             <div class="client__header-search">
                 <div class="header_logo">
-                    <img  height="50"
-                    src="https://www.shutterstock.com/image-vector/laser-cutting-logo-emblem-black-600nw-2261413647.jpg" />
+                    <img height="50"
+                        src="https://www.shutterstock.com/image-vector/laser-cutting-logo-emblem-black-600nw-2261413647.jpg" />
                 </div>
-                
+
                 <div class="header_input-search">
                     <input type="text" placeholder="tim kiem san pham.." class="form-control" />
                     <button>Search</button>
@@ -34,37 +34,34 @@
                 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
                     <div class="container-fluid">
                         <ul class="navbar-nav">
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">TRANG CHỦ</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">GIỚI THIỆU</a>
-                          </li>
-                          <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">SẢN PHẨM</a>
-                            <ul class="dropdown-menu">
-                              <li><a class="dropdown-item" href="#">Link</a></li>
-                              <li><a class="dropdown-item" href="#">Another link</a></li>
-                              <li><a class="dropdown-item" href="#">A third link</a></li>
-                            </ul>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">TÀI LIỆU</a>
-                          </li>  
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">LIÊN HỆ</a>
-                          </li>  
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">TRANG CHỦ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">GIỚI THIỆU</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown">SẢN PHẨM</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Link</a></li>
+                                    <li><a class="dropdown-item" href="#">Another link</a></li>
+                                    <li><a class="dropdown-item" href="#">A third link</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">TÀI LIỆU</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">LIÊN HỆ</a>
+                            </li>
                         </ul>
                     </div>
-                  </nav>
+                </nav>
             </div>
-        </div>
-        <div class="container">
-            @yield('content')
-        </div>
-
-
-        <div class="row client__footer">
+        </div>    
+        @yield('content')
+        <div class="row client__footer gx-0">
             <div class="col-lg-3">
                 <div class="client__footer-contact">
                     <p>Địa chỉ: 95 Hoàng Công Chất, Phường Phú Diễn, Quận Bắc Từ Liêm, Hà Nội</p>
@@ -81,7 +78,7 @@
             <div class="col-lg-3">
                 <iframe class="client__footer-map"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.586932641997!2d106.82607527417363!3d10.842889457966052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175215d2664e057%3A0xc125dc00dd84fa3b!2zUXXhuq1uIDk!5e0!3m2!1sen!2s!4v1701615789929!5m2!1sen!2s"
-                    width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"
+                     height="200" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
